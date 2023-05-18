9aef6a75c30f06e35579b844ab7e0caf65f61385
Rush-2K <syaidiazhari@gmail.com>
2023-05-18 16:22:15 +0800

    add firebase dependencies, add data to collection, delete data from docs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log("Hello this is my new website")
+import { initializeApp } from 'firebase/app'
+import { 
+    getFirestore, collection, getDocs,
+    addDoc, deleteDoc, doc
+ } from 'firebase/firestore'
+
+ const firebaseConfig = //your firebase configuration
+
+  // init firebase
+initializeApp(firebaseConfig)
+
+// initialize firestore services
+const db = getFirestore()
+
+//collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+    .then((snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+
+// adding docs
+// add the add form to the const
+const addBookForm = document.querySelector('.add')
+// attach eventlistener to the form
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
